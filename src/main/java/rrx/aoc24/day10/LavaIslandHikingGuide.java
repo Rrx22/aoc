6ae8be73cb302935e475cddf4e5f049618c98e4a
Rogier Roex <rogier.roex@gmail.com>
2024-12-11 23:37:25 +0100

    day10

---
+++ src/main/java/rrx/aoc24/day10/LavaIslandHikingGuide.java
@@ -0,0 +1,18 @@
+package rrx.aoc24.day10;
+
+import rrx.ChristmasAssert;
+import rrx.utils.FileUtil;
+
+class LavaIslandHikingGuide {
+
+    public static void main(String[] args) {
+        var topographicMap = FileUtil.readToGrid("24/d10p1");
+
+        TrailBlazer trailBlazer = new TrailBlazer()
+                .withGrid(topographicMap)
+                .withPopulatedTrailHeads()
+                .withPopulateRoutes();
+        ChristmasAssert.test(trailBlazer.totalSets(), 717L);
+        ChristmasAssert.test(trailBlazer.totalTrails(), 1686L);
+    }
+}
---
+++ src/main/java/rrx/aoc24/day10/TrailBlazer.java
@@ -0,0 +1,81 @@
+package rrx.aoc24.day10;
+
+import rrx.utils.Direction;
+import rrx.utils.GridUtil;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static rrx.utils.Direction.DOWN;
+import static rrx.utils.Direction.LEFT;
+import static rrx.utils.Direction.RIGHT;
+import static rrx.utils.Direction.UP;
+
+class TrailBlazer {
+
+    private char[][] grid;
+    private Map<String, HashMap<String, Integer>> trails;
+
+    public long totalSets() {
+        return trails.values().stream()
+                .mapToInt(Map::size)
+                .sum();
+    }
+
+    public long totalTrails() {
+        return trails.values().stream()
+                .flatMap(s -> s.values().stream())
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    public TrailBlazer withGrid(char[][] grid) {
+        this.grid = grid;
+        return this;
+    }
+
+    public TrailBlazer withPopulatedTrailHeads() {
+        this.trails = new HashMap<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '0') {
+                    trails.put(String.format("%d,%d", i, j), new HashMap<>());
+                }
+            }
+        }
+        return this;
+    }
+
+    public TrailBlazer withPopulateRoutes() {
+        for (var trailHead : trails.keySet()) {
+            var coords = Arrays.stream(trailHead.split(","))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            populateRoute(0, trailHead, coords[0], coords[1]);
+        }
+        return this;
+    }
+
+    private void populateRoute(int expectedVal, String trailHead, int row, int col) {
+        if (!GridUtil.isWithinBounds(row, col, grid.length, grid[0].length)) {
+            return;
+        }
+
+        int currVal = Character.getNumericValue(grid[row][col]);
+        if (currVal != expectedVal) {
+            return;
+        }
+
+        if (currVal == 9) {
+            var th = trails.get(trailHead);
+            th.compute(String.format("%d,%d", row, col), (_, v) -> (v == null) ? 1 : v + 1);
+            return;
+        }
+
+        populateRoute(currVal + 1, trailHead, row + RIGHT.y, col + RIGHT.x);
+        populateRoute(currVal + 1, trailHead, row + LEFT.y, col + LEFT.x);
+        populateRoute(currVal + 1, trailHead, row + UP.y, col + UP.x);
+        populateRoute(currVal + 1, trailHead, row + DOWN.y, col + DOWN.x);
+    }
+}
