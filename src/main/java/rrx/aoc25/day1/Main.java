80c0bcdfafdd97f4fe03a9aa9b4f25224f69ce8e
Rogier Roex <rogier.roex@gmail.com>
2025-12-01 21:34:40 +0100

    2025d1p2

--- src/main/java/rrx/aoc25/day1/Main.java
+++ src/main/java/rrx/aoc25/day1/Main.java
@@ -5,15 +5,7 @@ import rrx.utils.FileUtil;
 
 class Main {
     static void main() {
-
         DialLock dialLock = new DialLock(FileUtil.readFile("25/d01p1"));
-
-        long password = dialLock.turnDials();
-        ChristmasAssert.test(
-                password < 6668L
-                        && password != 6142L
-                        && password != 6568L
-                , password);
-
+        ChristmasAssert.test(dialLock.turnDials(), 6122L);
     }
 }
